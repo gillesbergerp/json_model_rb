465ac79048d285f22c7e404718d33ee4c0dfdeb6
Paul Gillesberger <paul.gillesberger@tractive.com>
2025-12-25 12:40:24 +0100

    Add additional numeric validations

--- lib/json_model/type_spec/primitive/numeric.rb
+++ lib/json_model/type_spec/primitive/numeric.rb
@@ -6,14 +6,38 @@ module JsonModel
       class Numeric < Primitive
         # @param [String] type
         # @param [Integer, nil] multiple_of
-        def initialize(type, multiple_of: nil)
+        # @param [Integer, nil] minimum
+        # @param [Integer, nil] maximum
+        # @param [Integer, nil] exclusive_minimum
+        # @param [Integer, nil] exclusive_maximum
+        def initialize(
+          type,
+          multiple_of: nil,
+          minimum: nil,
+          maximum: nil,
+          exclusive_minimum: nil,
+          exclusive_maximum: nil
+        )
           super(type)
           @multiple_of = multiple_of
+          @minimum = minimum
+          @maximum = maximum
+          @exclusive_minimum = exclusive_minimum
+          @exclusive_maximum = exclusive_maximum
         end
 
         # @return [Hash]
         def as_schema
-          super.merge({ multipleOf: @multiple_of }.compact)
+          super
+            .merge(
+              {
+                multipleOf: @multiple_of,
+                minimum: @minimum,
+                maximum: @maximum,
+                exclusiveMinimum: @exclusive_minimum,
+                exclusiveMaximum: @exclusive_maximum,
+              }.compact,
+            )
         end
 
         # @param [Symbol] name
@@ -21,6 +45,17 @@ module JsonModel
         def register_validations(name, klass)
           super
           register_multiple_of_validation(name, klass, @multiple_of)
+          if @minimum || @maximum || @exclusive_minimum || @exclusive_maximum
+            klass.validates_numericality_of(
+              name,
+              **{
+                greater_than: @exclusive_minimum,
+                less_than: @exclusive_maximum,
+                greater_than_or_equal_to: @minimum,
+                less_than_or_equal_to: @maximum,
+              }.compact,
+            )
+          end
         end
 
         private
@@ -36,7 +71,7 @@ module JsonModel
           klass.validate do |record|
             value = record.send(name)
             if value.present? && value % multiple_of != 0
-              record.errors.add(name, "must be a multiple of #{multiple_of}")
+              record.errors.add(name, :multiple_of, message: "must be a multiple of #{multiple_of}")
             end
           end
         end
--- spec/type_spec/primitive/numeric_spec.rb
+++ spec/type_spec/primitive/numeric_spec.rb
@@ -8,6 +8,26 @@ RSpec.describe(JsonModel::TypeSpec::Primitive::Numeric) do
       expect(described_class.new('number', multiple_of: 5).as_schema)
         .to(eq({ type: 'number', multipleOf: 5 }))
     end
+
+    it('includes the minimum attribute') do
+      expect(described_class.new('number', minimum: 5).as_schema)
+        .to(eq({ type: 'number', minimum: 5 }))
+    end
+
+    it('includes the maximum attribute') do
+      expect(described_class.new('number', maximum: 5).as_schema)
+        .to(eq({ type: 'number', maximum: 5 }))
+    end
+
+    it('includes the exclusiveMinimum attribute') do
+      expect(described_class.new('number', exclusive_minimum: 5).as_schema)
+        .to(eq({ type: 'number', exclusiveMinimum: 5 }))
+    end
+
+    it('includes the exclusiveMaximum attribute') do
+      expect(described_class.new('number', exclusive_maximum: 5).as_schema)
+        .to(eq({ type: 'number', exclusiveMaximum: 5 }))
+    end
   end
 
   describe('.register_validations') do
@@ -35,11 +55,115 @@ RSpec.describe(JsonModel::TypeSpec::Primitive::Numeric) do
         expect(instance.valid?)
           .to(be(false))
         expect(instance.errors.map(&:type))
-          .to(eq(['must be a multiple of 5']))
+          .to(eq(%i(multiple_of)))
       end
 
       it('succeeds the validation for integers') do
         instance = klass.new(foo: 12_345)
+
+        expect(instance.valid?)
+          .to(be(true))
+        expect(instance.errors)
+          .to(be_empty)
+      end
+    end
+
+    context('for minimum') do
+      before do
+        described_class
+          .new('number', minimum: 5)
+          .register_validations(:foo, klass)
+      end
+
+      it('fails the validation for invalid values') do
+        instance = klass.new(foo: 4)
+
+        expect(instance.valid?)
+          .to(be(false))
+        expect(instance.errors.map(&:type))
+          .to(eq(%i(greater_than_or_equal_to)))
+      end
+
+      it('succeeds the validation for valid values') do
+        instance = klass.new(foo: 5)
+
+        expect(instance.valid?)
+          .to(be(true))
+        expect(instance.errors)
+          .to(be_empty)
+      end
+    end
+
+    context('for maximum') do
+      before do
+        described_class
+          .new('number', maximum: 5)
+          .register_validations(:foo, klass)
+      end
+
+      it('fails the validation for invalid values') do
+        instance = klass.new(foo: 6)
+
+        expect(instance.valid?)
+          .to(be(false))
+        expect(instance.errors.map(&:type))
+          .to(eq(%i(less_than_or_equal_to)))
+      end
+
+      it('succeeds the validation for valid values') do
+        instance = klass.new(foo: 5)
+
+        expect(instance.valid?)
+          .to(be(true))
+        expect(instance.errors)
+          .to(be_empty)
+      end
+    end
+
+    context('for exclusive_minimum') do
+      before do
+        described_class
+          .new('number', exclusive_minimum: 5)
+          .register_validations(:foo, klass)
+      end
+
+      it('fails the validation for invalid values') do
+        instance = klass.new(foo: 5)
+
+        expect(instance.valid?)
+          .to(be(false))
+        expect(instance.errors.map(&:type))
+          .to(eq(%i(greater_than)))
+      end
+
+      it('succeeds the validation for valid values') do
+        instance = klass.new(foo: 6)
+
+        expect(instance.valid?)
+          .to(be(true))
+        expect(instance.errors)
+          .to(be_empty)
+      end
+    end
+
+    context('for exclusive_maximum') do
+      before do
+        described_class
+          .new('number', exclusive_maximum: 5)
+          .register_validations(:foo, klass)
+      end
+
+      it('fails the validation for invalid values') do
+        instance = klass.new(foo: 5)
+
+        expect(instance.valid?)
+          .to(be(false))
+        expect(instance.errors.map(&:type))
+          .to(eq(%i(less_than)))
+      end
+
+      it('succeeds the validation for valid values') do
+        instance = klass.new(foo: 4)
 
         expect(instance.valid?)
           .to(be(true))
